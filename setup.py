05f6a55333f62dd37cac40fad40e5d285d54cd2d
LiviuTofan <liviutofan@mail.ru>
2025-05-07 03:01:05 +0300

    Added setup to project

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-# Setup
+from setuptools import setup, find_packages
+
+setup(
+    name="go2web",
+    version="1.0.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'go2web=main:main',
+        ],
+    },
+    description="A web utility tool for fetching and parsing web content",
+    author="Go2Web Developer",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
