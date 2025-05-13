407d09e8266ef7429a20eee231d903e0fe921b67
LiviuTofan <liviutofan@mail.ru>
2025-05-13 23:35:24 +0300

    changed search handler to give real link decoded, not redirect one

--- go2web/search/engine.py
+++ go2web/search/engine.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus, parse_qs, urlparse, unquote
 from bs4 import BeautifulSoup
 from core.client import HTTPClient
 
@@ -12,6 +12,12 @@ def handle_search(term):
         soup = BeautifulSoup(body, 'html.parser')
         results = soup.find_all('a', class_='result__a', limit=10)
         for i, a in enumerate(results, 1):
-            print(f"{i}. {a.get_text(strip=True)}\n   {a['href']}")
+            href = a['href']
+            if href.startswith('//'): # //duckduckgo.com...
+                href = 'https:' + href # https://duckduckgo.com...
+            parsed_href = urlparse(href)
+            uddg = parse_qs(parsed_href.query).get('uddg', [None])[0]
+            real_url = unquote(uddg) if uddg else href
+            print(f"{i}. {a.get_text(strip=True)}\n   {real_url}")
     except Exception as e:
         print(f"Search failed: {e}")
